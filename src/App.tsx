34ec6f454ca75e9bb5deda44ef3fd175f6a0ffeb
Andrew Davis <dynamicdebug@gmail.com>
2024-09-25 12:43:03 -0400

    add some game state game components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [showGameModal, setShowGameModal] = useState(false);
+  const [showGameLostModal, setShowGameLostModal] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
   return (
-    <aside>
-      <h1>Connections Clone</h1>
-    </aside>
+    <div className="flex flex-col items-center w-11/12 md:w-3/4 lg:w-7/12 mx-auto mt-14">
+      <h1 className="text-black text-4xl font-semibold my-4 ml-4">
+        Connections
+      </h1>
+      <hr className="mb-4 md:mb-4 w-full" />
+      <h2 className="text-black mb-4 text-2xl">Create four groups of four</h2>
+    </div>
   );
 }
 
